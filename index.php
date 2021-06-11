9b95af74c28ff0d23c4c929df4ce84421d1a78f0
Kevin Sunjaya <kssunjaya@gmail.com>
2021-06-11 09:28:30 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>FUN Resort Park</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body style="margin: 0px;">
+    <span class="header" style="margin:10px;">
+        <img src="src/logo.png" alt="logo" id="logo">
+    </span>
+    <div>
+        <img src="src/sleeping-beauty-castle-tint-16x9.webp" alt="banner" style="width:100%;">
+    </div>
+</body>
+</html>
