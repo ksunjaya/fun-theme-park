378db777a4472dc0b9e737dce1324bd5a220f79d
Kevin Sunjaya <kssunjaya@gmail.com>
2021-06-12 15:06:14 +0700

    update controller
    Buat bisa buka si pengunjung aja dulu

--- controller/services/view.php
+++ controller/services/view.php
@@ -1,7 +1,8 @@
 <?php
 
 class View {
-    public static function createView ($view, $param)
+    //yang ini khusus buat pengunjung aja, ntar bikin function baru buat admin
+    public static function createPengunjungView ($view, $param)
     {
         foreach ($param as $key => $value)
         {
@@ -13,7 +14,7 @@ class View {
         ob_end_clean();
 
         ob_start();
-        include 'view/layout/layout.php';
+        include 'view/layout/layout_pengunjung.php';
         $include = ob_get_contents();
         ob_end_clean();
         return $include;
