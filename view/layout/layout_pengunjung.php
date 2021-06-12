2e8e73df60f1b1dfcfbf9320f1d85a5b081822a6
Vincent Kurniawan <itsvincentk56@gmail.com>
2021-06-12 16:05:18 +0700

    pengunjung

--- view/layout/layout_pengunjung.php
+++ view/layout/layout_pengunjung.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <link rel="stylesheet" href="view/css/style_pengunjung.css">
-    <link rel="stylesheet" href="view/css/style_pengunjung.css">
+    <link rel="stylesheet" href="view/css/style_home.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Lato:wght@700;900&display=swap');
     </style>
