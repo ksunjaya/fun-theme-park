1039c33d7ba8ee352e323ebf3f6a8efd21358b79
Kevin Sunjaya <kssunjaya@gmail.com>
2021-06-12 15:55:40 +0700

    Update home.php

---
+++ view/home.php
@@ -0,0 +1,9 @@
+<div class="parent-flex">
+  <div class="left-container">
+    <h1 class="blue" id="title">Watch Dreams Come <br>True at the Happiest <br>Place on Earth</h1>
+    
+  </div>
+  <div class="right-container">
+
+  </div>
+</div>
