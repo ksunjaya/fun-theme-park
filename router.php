db9be56a37684b2497bf5ce214982ff3dd1dac42
Kevin Sunjaya <kssunjaya@gmail.com>
2021-06-12 14:24:26 +0700

    template view

--- router.php
+++ router.php
@@ -4,7 +4,12 @@
 	$baseURL = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; 
 
 	if($_SERVER["REQUEST_METHOD"] == "GET"){
-		
+		switch($url){
+			case $baseURL.'/home':
+				break;
+			default:
+				echo 'Page not found';
+		}
 	}else if($_SERVER["REQUEST_METHOD"] == "POST"){
 		
 	}
