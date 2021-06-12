3a329efa127a32a25465409b515d42ae35dc3667
Kevin Sunjaya <kssunjaya@gmail.com>
2021-06-12 16:04:38 +0700

    update home

--- view/home.php
+++ view/home.php
@@ -2,6 +2,7 @@
   <div class="left-container">
     <h1 class="blue" id="title">Watch Dreams Come <br>True at the Happiest <br>Place on Earth</h1>
     
+    <a id="about" href="about">PELAJARI LEBIH LANJUT</a>
   </div>
   <div class="right-container">
 
--- view/layout/layout_pengunjung.php
+++ view/layout/layout_pengunjung.php
@@ -2,9 +2,10 @@
 <html>
 <head>
     <link rel="stylesheet" href="view/css/style_pengunjung.css">
-    <link rel="stylesheet" href="view/css/style_pengunjung.css">
+    <link rel="stylesheet" href="view/css/style_home.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Lato:wght@700;900&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@700&family=Lato:wght@700;900&display=swap');
     </style>
 </head>
 <body>
