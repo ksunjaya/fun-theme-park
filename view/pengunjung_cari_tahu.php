e8f756c996377385f897446da1e092527a85b16c
Vincent Kurniawan <itsvincentk56@gmail.com>
2021-06-12 15:42:39 +0700

    pengunjung

---
+++ view/pengunjung_cari_tahu.php
@@ -0,0 +1,34 @@
+<div id="main">
+    <div class="row-1">
+        <img id = "img_castle" src = "src/castle.jpg">
+        <div class="center_castle_text">Magic Is Here<br>The FUN Theme Park is Now Open!</div>
+    </div>
+    <div class="row-2">
+        <div class="inline">
+            <h1>About Us</h1>
+            <div>FUN Theme Park merupakan sebuah theme park terbesar di Bandung yang memiliki berbagai wahana yang cocok bagi anak-anak maupun dewasa sehingga dapat menjadi pilihan tempat hiburan keluarga. </div>
+            <h2>Kunjungi dan rasakan keajaibannya!</h2>
+        </div>
+        <div class="inline">
+            <img src="src/distancing-carousel-1x1.webp">
+        </div>
+        <div class="inline"> 
+            <h1>LOKASI</h1>
+            <p>Jalan Raya Imajiner Parahyangan no. 15<br>Bandung<br>Indonesia</p>
+            <h1>CONTACT US</h1>
+            <p>0899-999-9999<br>halo</p>
+        </div>
+        <div class="inline">
+            
+        </div>
+        <div class="inline">
+            
+        </div>
+    </div>
+    <div class="row-3">
+    
+    </div>
+    <div class="footer">
+    
+    </div>
+</div>
