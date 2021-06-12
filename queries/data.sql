f025aa18453dfd2c80641716f9bee5e12a20317f
sharonkeziaf <6181901006@student.unpar.ac.id>
2021-06-12 15:25:47 +0700

    Insert Data

---
+++ queries/data.sql
@@ -0,0 +1,69 @@
+INSERT INTO Harga_Tiket
+VALUES ('2021-07-01', 40000);
+
+INSERT INTO Harga_Tiket
+VALUES ('2021-07-02', 40000);
+
+INSERT INTO Harga_Tiket
+VALUES ('2021-07-03', 60000);
+
+INSERT INTO Harga_Tiket
+VALUES ('2021-07-04', 60000);
+
+INSERT INTO Harga_Tiket
+VALUES ('2021-07-05', 60000);
+
+
+
+INSERT INTO Limit_Tiket
+VALUES ('2021-07-01', 400, 5, 211);
+
+INSERT INTO Limit_Tiket
+VALUES ('2021-07-02', 400, 5, 213);
+
+INSERT INTO Limit_Tiket
+VALUES ('2021-07-03', 500, 6, 122);
+
+INSERT INTO Limit_Tiket
+VALUES ('2021-07-04', 500, 6, 98);
+
+INSERT INTO Limit_Tiket
+VALUES ('2021-07-05', 500, 6, 80);
+
+
+
+INSERT INTO Pemilik 
+VALUES ('1384281900120541', 'Willy Wonka', 'user_willy', 'thewilwon123');
+
+
+
+INSERT INTO Karyawan
+VALUES ('1384281923761014', 'Wombat Wonderland', 'woland', 'wombwonder01');
+
+INSERT INTO Karyawan
+VALUES ('1384281923761034', 'Dodo Digidigi Bang', 'bangdodo', 'dordor2021');
+
+INSERT INTO Karyawan
+VALUES ('1384281923761001', 'Bambang Budiman', 'Bambankxs', 'bambu1020');
+
+
+
+INSERT INTO Pengunjung
+VALUES ('2233114112100123', 'Lala Lilili', '081902193103');
+
+INSERT INTO Reservasi
+VALUES (29022101, 3, '2233114112100123', '2021-07-01');
+
+INSERT INTO Transaksi
+VALUES (20210001, 29022101, '2021-07-01', 120000);
+
+
+
+INSERT INTO Pengunjung
+VALUES ('2233114112112003', 'Si Wombomz', '081681901002');
+
+INSERT INTO Reservasi
+VALUES (29022102, 3, '2233114112112003', '2021-07-04');
+
+INSERT INTO Transaksi
+VALUES (20210002, 29022102, '2021-07-04', 180000);
