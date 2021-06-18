41f85ffe815518e5086e115cf87e436a58ed6e4d
Vincent Kurniawan <itsvincentk56@gmail.com>
2021-06-18 08:41:43 +0700

    a

--- view/pemilik_main.php
+++ view/pemilik_main.php
@@ -1,14 +1,14 @@
 <div id = "login-form" class="login-main" action="" method="POST">
     <div class="login-content bg-white">
-        <div class="login-h1-box" style="height: 30%;">
+        <div class="login-h1-box" style="height: 25%;">
             <h1 class="c-dark-blue fs-48">HOME</h1>
         </div>
-        <div class="login-isi" style="height: 40%;">
-           <a href = ""class="button main-margin"><h2>STAFF ACCOUNT</h2></a>
-           <a href = ""class="button main-margin"><h2>UPDATE TICKET</h2></a>
-           <a href = ""class="button main-margin"><h2>VIEW TRANSACTION</h2></a>
+        <div class="login-isi" style="height: 50%;">
+           <a href = ""class="button main-margin-button"><h2>STAFF ACCOUNT</h2></a>
+           <a href = ""class="button main-margin-button"><h2>UPDATE TICKET</h2></a>
+           <a href = ""class="button"><h2>VIEW TRANSACTION</h2></a>
         </div>
-        <div class="login-isi" style="height: 30%;">
+        <div class="login-isi" style="height: 25%;">
             <a href = ""class="footer-button">SIGN OUT</a>
         </div>
     </div>
