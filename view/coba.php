736ab1b0f9ba04e2ca472f74729bd09cad9def9c
Vincent Kurniawan <itsvincentk56@gmail.com>
2021-06-18 22:08:40 +0700

    benerin coba.php (buat create account)

--- view/coba.php
+++ view/coba.php
@@ -4,25 +4,25 @@
             <h1 class="c-dark-blue fs-48">CREATE</h1>
         </div>
         <div class="login-isi" style="height: 70%;">
-            <div class="login-box" style="margin-bottom: 25px;">
+            <div class="login-box" style="margin-bottom: 20px;">
                 <label class="fw-700 fs-18 c-dark-blue">ID NUMBER (KTP)</label>
-                <input name="username" id = "ktp" class="login-input fw-700 fs-36 bg-light-blue" type="text" maxlength="16">
+                <input name="ktp" id = "ktp" class="login-input fw-700 fs-36 bg-light-blue" type="text" maxlength="16">
             </div>
-            <div class="login-box" style="margin-bottom: 25px;">
+            <div class="login-box" style="margin-bottom: 20px;">
                 <label class="fw-700 fs-18 c-dark-blue">FULL NAME</label>
-                <input name="username" id = "fullname" class="login-input fw-700 fs-36 bg-light-blue" type="text">
+                <input name="fullname" id = "fullname" class="login-input fw-700 fs-36 bg-light-blue" type="text">
             </div>
-            <div class="login-box" style="margin-bottom: 25px;">
+            <div class="login-box" style="margin-bottom: 20px;">
                 <label class="fw-700 fs-18 c-dark-blue">USERNAME</label>
                 <input name="username" id = "username" class="login-input fw-700 fs-36 bg-light-blue" type="text">
             </div>
-            <div class="login-box" style="margin-bottom: 25px;">
+            <div class="login-box" style="margin-bottom: 20px;">
                 <label class="fw-700 fs-18 c-dark-blue">PASSWORD</label>
                 <input name="username" id = "password" class="login-input fw-700 fs-36 bg-light-blue" type="password">
             </div>
-            <div class="login-box">
+            <div class="login-box" style="margin-bottom: 20px;">
                 <label class="fw-700 fs-18 c-dark-blue">PHOTO</label>
-                <input name="password" id = "photo" class="login-input fw-700 fs-36 bg-light-blue" type="file">
+                <input name="photo" id = "photo" class="login-input fw-700 fs-36 bg-light-blue" type="file">
             </div>
         </div>
         <div class="footer-box-button" style="height: 15%;">
