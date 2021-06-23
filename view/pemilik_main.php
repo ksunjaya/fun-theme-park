1972a6a92f6fb328b2c03a6fff73a64995c21bf5
Vincent Kurniawan <itsvincentk56@gmail.com>
2021-06-23 17:07:19 +0700

    nambah username di main

--- view/pemilik_main.php
+++ view/pemilik_main.php
@@ -1,5 +1,10 @@
 
 <div id = "login-form" class="login-main" action="" method="POST">
+
+    <div class="nama-user">
+        <span class="material-icons md-36" style="margin-right: 10px;">account_box</span>
+        <h1 style="margin-right: 50px; font-size: 36px; font-weight: 700;"><?php echo $nama_user?></h1>
+    </div>
     <div class="login-content bg-white">
         <div class="login-h1-box" style="height: 25%;">
             <h1 class="c-dark-blue fs-48">HOME</h1>
