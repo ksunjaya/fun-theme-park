92f7a4456001d9767b86d2beeddbac0d15efa1c6
Vincent Kurniawan <itsvincentk56@gmail.com>
2021-06-26 00:11:59 +0700

    chart

--- view/layout/layout_admin.php
+++ view/layout/layout_admin.php
@@ -28,55 +28,5 @@
     <?php
         echo $content;
     ?>
-    <canvas id="chart" width="400" height="400"></canvas>
-    <script>
-        let ctx = document.getElementById('chart').getContext('2d');
-        let arrTanggal = [];
-        let arrCust = [];
-        <?php
-            foreach ($resultChart as $key=>$value){
-                ?>
-                arrTanggal.push(<?php echo $value["tanggal"]?>);
-                arrCust.push(<?php echo $value["sum"]?>);
-        <?php
-            }
-        ?>
-        let myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: arrTanggal,
-                datasets: [{
-                    label: 'Data Jumlah Pengunjung per Hari ',
-                    data: arrCust,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-        
-        
-    </script>
 </body>
 </html>
